8a8ff35032392daed1892b722c807a4efc48d62c
arya <arya20498@iiitd.ac.in>
2021-12-20 05:19:19 +0530

    commit

--- src/main/java/com/example/project/Controller.java
+++ src/main/java/com/example/project/Controller.java
@@ -1,7 +1,7 @@
 package com.example.project;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -11,46 +11,78 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
-
-
+    private boolean isRightTurn = true;
     private Stage stage;
-    private Pane gameScreen;
-    private Scene scene;
+    private Scene gameScene;
+    private Parent root;
+    private int blueSqNumber = 0;
+    private int greenSqNumber = 0;
 
     @FXML
-    public void switchToGame(ActionEvent e) throws IOException {
+    private ImageView grid;
 
+    @FXML
+    private ImageView greenPlayer;
+
+    @FXML
+    private ImageView playerBar;
+    @FXML
+    private ImageView diceImg;
+
+    public void switchtoMenu(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-
-
-        StackPane gameScreen = new StackPane();
-        setupGameScreen(gameScreen);
-        Scene scene = new Scene(gameScreen, 450, 600);
-        stage.setScene(scene);
+        gameScene = new Scene(root);
+        stage.setScene(gameScene);
         stage.show();
     }
 
-    public void setupGameScreen(StackPane pane) throws IOException {
+    public void switchtoGame(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        gameScene = new Scene(root);
+        stage.setScene(gameScene);
+        stage.show();
+        //boxes.add(r0);
+    }
 
-        // Back button setup
-        Button backButton = new Button("<-");
-        backButton.setTranslateX(-180);
-        backButton.setTranslateY(-260);
-        pane.getChildren().add(backButton);
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        backButton.setOnAction(e -> stage.setScene(scene));
 
-        // Board setup
-        //Image img = new Image("gameBoard.png");
+
+    public void handleDiceClick(ActionEvent e) throws IOException{
+        double originX = grid.getLayoutX() + grid.getFitHeight();
+        double originY = grid.getLayoutY();
+        double yCoordinate = (grid.getFitHeight()/10.0)/2.0;
+        int randInt = (int) Math.floor(6 * Math.random() +1);
+        //file:/C:/Users/tarun/IdeaProjects/ap-project/src/die-3.png
+        String die = "file:/C:/Users/tarun/IdeaProjects/ap-project/src/"+"die-" + randInt+ ".png";
+        Image die_img = new Image(die);
+        diceImg.setImage(die_img);
+
+        String player_bar_path;
+        if (isRightTurn) {
+            //green's turn
+            greenSqNumber += randInt;
+            yCoordinate -= Math.floor((greenSqNumber-1)/10)*(grid.getFitHeight()/10.0);
+            greenPlayer.setY(yCoordinate);
+            player_bar_path = "file:/C:/Users/tarun/IdeaProjects/ap-project/src/left-turn.png";
+            isRightTurn = false;
+        }
+        else {
+            player_bar_path = "file:/C:/Users/tarun/IdeaProjects/ap-project/src/right-turn.png";
+            isRightTurn = true;
+        }
+        Image bar_img = new Image(player_bar_path);
+        playerBar.setImage(bar_img);
+
+
 
     }
 }
--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -2,24 +2,39 @@ package com.example.project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    public static int box_width = 53;
+    public static int box_height = 98;
+    public static int num = 10;
+
+    private Group boxes = new Group();
+
+    private Parent createGraphics() {
+        Pane root = new Pane();
+
+        return root;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Snake and Ladder");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
